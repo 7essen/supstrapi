71938cbbced3fe87b438422ae2d647db9782065c
7essen <hussona4635@gmail.com>
2024-08-04 15:18:25 +0300

    dgsdgsgggsdg

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'postgres'),
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'aws-0-us-east-1.pooler.supabase.com'),
+      host: env('DATABASE_HOST', 'aws-0-us-west-1.pooler.supabase.com'),
       port: env.int('DATABASE_PORT', 6543),
       database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres.brtlckuprzdtzytcccvh'),
+      user: env('DATABASE_USERNAME', 'postgres.nyyfxfxmqhuuudmaaaql'),
       password: env('DATABASE_PASSWORD', 'Husseinhossam7#'),
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: false, // تأكد من إعداد SSL بشكل صحيح
       },
     },
     debug: false,
