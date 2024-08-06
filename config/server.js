b8a92d6664bc7585b3cf6b74443211b1b7a4d544
7essen <hussona4635@gmail.com>
2024-08-06 18:28:20 +0300

    hi

--- config/server.js
+++ config/server.js
@@ -1,12 +1,15 @@
-module.exports = ({ env }) => {
+module.exports = () => {
   return {
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
+    host: '0.0.0.0',
+    port: 1337,
     app: {
-      keys: env.array('APP_KEYS'),
+      keys: [
+        'HqosElJ05MZ9CscH9k9/ML/Yk4bZ2pMJ8A9/ZgVRSgK=',
+        'JZ7S8MgjRz1XH8GkD4zd+G8xPQ1VY5nt6SOzz9D0mgQ=',
+      ],
     },
     webhooks: {
-      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+      populateRelations: false,
     },
   };
 };
